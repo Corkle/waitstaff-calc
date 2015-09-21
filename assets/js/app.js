2ad5df4d31546e373b4777cf9099c12aca1c9c1c
Corey McDaniel <corey.r.mcdaniel@live.com>
2015-09-20 23:24:38 -0700

    Added feature for Earning Info calculation. Added calculator reset button.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,44 @@
 angular.module('calcApp', ['ui.utils.masks'])
     .controller('CalcCtrl', function () {
-        this.customer = {
-            subtotal: 0,
-            tip: 0,
-            total: function () {
+        function Customer() {
+            this.subtotal = 0;
+            this.tip = 0;
+            this.total = function () {
                 return this.subtotal + this.tip;
-            }
-        };
+            };
+        }
+
+        function Earnings() {
+            this.tipTotal = 0;
+            this.mealCount = 0;
+            this.averageTip = function () {
+                return this.tipTotal / this.mealCount || 0;
+            };
+        }
+
+        this.customer = new Customer();
+        this.earnings = new Earnings();
 
 
         this.calcMeal = function (form) {
             this.customer.subtotal = this.mealPrice * (1 + this.taxRate);
             this.customer.tip = this.customer.subtotal * this.tipPercent;
-        
+
+            this.earnings.tipTotal += this.customer.tip;
+            this.earnings.mealCount++;
+
             this.mealPrice = "";
             this.taxRate = "";
             this.tipPercent = "";
             form.$setPristine();
         };
+
+        this.resetCalc = function () {
+            this.customer = new Customer();
+            this.earnings = new Earnings();
+        };
     });
 
-
-
-
-
-
-
-
-
-var DEBUG = function (item) {
+function DEBUG(item) {
     console.log(item);
-};
+}
